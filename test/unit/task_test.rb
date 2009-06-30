ba3001306e9734ec0abf077b1f2a72286e98cbcb
Łukasz Bandzarewicz <lucassus@gmail.com>
2009-06-30 15:10:40 +0200

    Add test for Project and Task.

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should_belong_to :project
+  should_validate_presence_of :name
 end
